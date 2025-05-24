1d2ba7d1ca2978e2cacdc108667fae77af6b0685
TJDeTalente <thepurplewalrusgaming@gmail.com>
2025-05-24 12:51:44 -0500

    Add files via upload

---
+++ funky_code.py
@@ -0,0 +1,32 @@
+char = "#"
+
+print(f" {char*8}")
+print(f" {char*2}")
+print(f" {char*2}")
+print(f" {char*2}")
+print(f" {char*8}")
+print("")
+
+print(f" {char*8}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*8}")
+print("")
+print(f" {char*8}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*2}" , "   " , f"{char*2}")
+print(f" {char*2}" , "    " , f"{char*2}")
+print(f" {char*2}" , "   " , f"{char*2}")
+print(f" {char*2}" , "  " , f"{char*2}")
+print(f" {char*2}" , " " , f"{char*2}")
+print(f" {char*2}" , "" , f"{char*2}")
+print("")
+print(f" {char*8}")
+print(f" {char*2}")
+print(f" {char*8}")
+print(f" {char*2}")
+print(f" {char*8}")
+
+
