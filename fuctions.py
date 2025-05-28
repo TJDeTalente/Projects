8f77e83c955b8f8624f8fcd4b218024985a8ee72
TJDeTalente <thepurplewalrusgaming@gmail.com>
2025-05-27 21:16:24 -0500

    Add files via upload

---
+++ fuctions.py
@@ -0,0 +1,33 @@
+# def dollarizer():
+#     x = input("Dollarize ")
+#     s = x.replace("s", "$")
+#     return s
+#
+# print(dollarizer())
+
+# def eurizer():
+#     x = input("Eurize: ")
+#     s = x.replace("e" , "€")
+#     return s
+#
+# print(eurizer())
+
+# def celsius_to_fahrenheit(celsius):
+#     fahrenheit = (celsius * 9 / 5) + 32
+#     return fahrenheit
+#
+# print(celsius_to_fahrenheit(32))
+
+# def age_in_days():
+#     a = (int(input("Enter your age: ")))
+#     b = a * 365
+#     return b
+#
+# print(age_in_days())
+
+# def simple_interest(P, R, T):
+#     SI = SI = P * R * T
+#     return SI
+#
+# print(simple_interest(35, 45,55))
+
---
+++ shapes.py
@@ -0,0 +1,19 @@
+# def circle(r):
+#     radius = r * 2
+#     return radius
+#
+# def square_perimeter(L):
+#     p = L * 4
+#     return p
+#
+# def geometry():
+#     i = int(input("Enter the side length of the square "))
+#     square = i * 4
+#     circle_input = int(input("Enter the radius of the circle: "))
+#     radius = circle_input / 2
+#     if square > radius:
+#         print("The square has a larger perimeter than the circle.")
+#     else:
+#         print("The circle has a larger radius.")
+#
+# print(geometry())
